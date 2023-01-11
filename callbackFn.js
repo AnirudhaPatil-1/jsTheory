91a68dae2eff4c3231621d431ec6a070641d19a6
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-01-11 22:35:07 +0530

    working of callback function

--- callbackFn.js
+++ callbackFn.js
@@ -4,7 +4,12 @@ function print(value){
 
 function sum(a, b, print){
     let c = a + b;
-    print(c);
+    print(c );
 }
 
 sum(2, 3, print);
+
+//working of callback fn 
+//JS is single threaded. has a stack when get to callback fn it is moved to remote api
+// after that the fn is moved to queue where the it is checked by ** ** where the stack is empty
+//if yes the callback fn is moved to stack and then executed
