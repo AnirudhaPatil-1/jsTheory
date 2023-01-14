aeb4e0a4888764506fc284bb74f99847cf5d7c55
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-01-14 16:45:43 +0530

    promises questions

--- asynchronous/promise.js
+++ asynchronous/promise.js
@@ -8,6 +8,8 @@
 // });
 // console.log(isItDone);// O/P -> Promise { 'work is done' }
 
+// const { promise } = require("selenium-webdriver");
+
 //2. 
 // let cond = false;
 // let isItDone = new Promise(function (resolve, reject){
@@ -20,23 +22,86 @@
 // console.log(isItDone);
 // //O/P -> Promise { <rejected> 'work is not done' }
 
-let cond = false;
-let isItDone = new Promise(function(resolve, reject){
-    if(cond){
-        resolve("work is done");
-    }else{
-        reject("work is not done");
-    }
-});
-isItDone.then(function(val){
-    console.log(val);
-    //O/P -> work is done
-}).catch(function(err){
-    console.log(err);
-    console.log("Error1");
-    throw new Error("i am new error");
-    //O/P for cond = false => work is not done
-}).catch(function(error){
-    console.log(error);
-    console.log("printed new thrown error")
-})
+//3.
+// let cond = false;
+// let isItDone = new Promise(function(resolve, reject){
+//     if(cond){
+//         resolve("work is done");
+//     }else{
+//         reject("work is not done");
+//     }
+// });
+// isItDone.then(function(val){
+//     console.log(val);
+//     //O/P -> work is done
+// }).catch(function(err){
+//     console.log(err);
+//     console.log("Error1");
+//     throw new Error("i am new error");
+//     //O/P for cond = false => work is not done
+// }).catch(function(error){
+//     console.log(error);
+//     console.log("printed new thrown error")
+// })
+
+//4. 
+// function job(state){
+//     return new Promise(function(resolve, reject){
+//         if(state){
+//             resolve("success");
+//         }else{
+//             reject("error");
+//         }
+//     });
+// }
+
+// let promise = job(true);
+
+// promise.then(function(data){
+//     console.log(data);
+//     return job(false);
+// }).catch(function(error){
+//     console.log(error);
+//     return "error caught";
+// }).then(function(data){
+//     console.log(data);
+//     return job(true);
+// }).catch(function(error){
+//     console.log(error);
+// })
+// //OUTPUT ->
+// // success
+// // error
+// // error caught 
+
+//5.
+// let cond = true;
+// setInterval(() =>{
+//     if(cond){
+//         console.log("hello");
+//     }
+// }, 2000);
+
+// setTimeout(() => {
+//     cond = false;
+// }, 10000);
+
+// // 6.
+// promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject("some result");
+//     }, 1000);
+// })
+
+// promise.then((val) => {
+//     console.log("then: " + val);
+// }).catch((val) => {
+//     console.log("catch: " + val);
+// });
+
+// console.log("hello1");
+// console.log("hello2");
+// //OUTPUT => 
+// // hello1
+// // hello2
+// // catch: some result
