e8b28362609d6ac78deb59360341b62b8f0225b5
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-01-23 00:01:40 +0530

    pratice problem Promise.all

--- asynchronous/promise.all.js
+++ asynchronous/promise.all.js
@@ -34,3 +34,42 @@ const timeOut = (t) => {
 
 timeOut(1000).then(result => console.log(result));
 Promise.all([timeOut(1000), timeOut(2000)]).then(result => console.log(result)) 
+
+
+//
+require("chromedriver");
+
+let wd = require("selenium-webdriver");
+let chrome = require("selenium-webdriver/chrome");
+let browsers = [new wd.Builder().forBrowser('chrome').build(), new wd.Builder().forBrowser('chrome').build(), new wd.Builder().forBrowser('chrome').build(), new wd.Builder().forBrowser('chrome').build(),new wd.Builder.forBrowser('chrome').build(),new wd.Builder().forBrowser('chrome').build()];
+
+let urls = ["https://www.youtube.com", "https://www.facebook.com", "https://www.youtube.com", "https://www.youtube.com", "https://www.youtube.com", "https://www.facebook.com"];
+let arr = [];
+let arr1 = [];
+
+for(let i in urls){
+    arr.push(browsers[i].get(urls[i]));
+}
+
+async function main(){
+    await Promise.all(arr);
+    console.log("all websites opened");
+    for(let i in browsers){
+        arr1.push(browsers[i].close());
+    }
+    await Promise.all(arr1);
+    console.log("all browser closed");
+}
+
+
+//2. 
+const timeOut = (t) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`completed in ${t}`)
+        }, t)
+    })
+}
+
+timeOut(1000).then(result => console.log(result));
+Promise.all([timeOut(1000), timeOut(2000)]).then(result => console.log(result)) 
